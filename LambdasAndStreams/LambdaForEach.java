5699f6224a25fb4b3dc2efbe590a3fb9cf056f46
madhupriyaallam <madhupriya.allam@zemosolabs.com>
2022-02-11 10:34:10 +0530

    Java Assignments

---
+++ LambdasAndStreams/LambdaForEach.java
@@ -0,0 +1,13 @@
+package Java.LambdasAndStreams;
+import java.util.ArrayList;
+public class LambdaForEach {
+        public static void main(String[] args) {
+            ArrayList<Integer> numbers = new ArrayList<Integer>();
+            numbers.add(5);
+            numbers.add(9);
+            numbers.add(8);
+            numbers.add(1);
+            numbers.forEach( (n) -> { System.out.println(n); } );
+        }
+    }
+
